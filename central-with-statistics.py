9660be7bc6f448aa8543ebff1f018f261bd8c0d4
eden0214 <89224319+eden0214@users.noreply.github.com>
2021-08-31 17:07:49 -0500

    Add files via upload

---
+++ central-with-statistics.py
@@ -0,0 +1,18 @@
+# """Module 2 Project to determine central tendencies."""
+
+values = [47, 95, 88, 73, 88, 84]
+
+# Calculate the count of the values
+len(values)
+    
+# Calculate the sum of the values
+sum(values)
+
+# Calculate the mean of the values
+statistics.mean(values)
+
+# Calculate the median of the values
+statistics.median(values)
+
+# Calculate the mode of the values
+statistics.mode(values)
